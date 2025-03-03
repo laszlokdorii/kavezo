eb767f6040583d5709c61595edfdc4d23c348e6c
laszlokdorii <laszlok.dora@gmail.com>
2025-03-03 18:12:31 +0100

    validator

--- src/kavezo/dto/create-kavezo.dto.ts
+++ src/kavezo/dto/create-kavezo.dto.ts
@@ -1,1 +1,44 @@
-export class CreateKavezoDto {}
+import { IsBoolean, IsDate, IsInt, IsNotEmpty, IsString, ValidatorConstraint, ValidatorConstraintInterface, ValidationArguments, registerDecorator, ValidationOptions} from "class-validator";
+
+@ValidatorConstraint({ name: 'isFutureDate', async: false })
+class IsFutureDateConstraint implements ValidatorConstraintInterface {
+  validate(date: Date, args: ValidationArguments) {
+    const currentDate = new Date();
+    return date >= currentDate; 
+  }
+
+  defaultMessage(args: ValidationArguments) {
+    return 'A dátum nem lehet a múltban!';
+  }
+}
+
+function IsFutureDate(validationOptions?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      constraints: [],
+      validator: IsFutureDateConstraint,
+    });
+  };
+}
+
+export class CreateKavezoDto {
+    @IsString()
+    @IsNotEmpty()
+    felleponev: string;
+
+    @IsDate()
+    @IsNotEmpty()
+    @IsFutureDate()
+    kezdesido: Date;
+
+    @IsInt()
+    @IsNotEmpty()
+    idotartam: number;
+
+    @IsBoolean()
+    @IsNotEmpty()
+    elmarad:boolean;
+}
